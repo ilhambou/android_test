7c6159e866ff3943cc4a7e3ffb612d8468bb04a0
ilhambou <82772375+ilhambou@users.noreply.github.com>
2023-03-31 10:19:47 +0000

    tp

--- app/src/main/java/com/example/myapplicationn/MainActivity.java
+++ app/src/main/java/com/example/myapplicationn/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplicationn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ClickHandler(View view) {
+        Toast.makeText(this,"Button clické",Toast.LENGTH_SHORT).show();
     }
 }
