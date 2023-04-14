bfc28b6b2ed2a5a03145c25badf00d23d7957ee0
ilhambou <82772375+ilhambou@users.noreply.github.com>
2023-04-14 10:09:43 +0000

    data2

---
+++ app/src/main/java/com/example/myapplicationn/Etudiant.java
@@ -0,0 +1,66 @@
+package com.example.myapplicationn;
+
+import android.graphics.Bitmap;
+
+public class Etudiant {
+    private int id;
+    private String nom, prenom,classe,phone;
+    private Bitmap photo;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public Bitmap getPhoto() {
+        return photo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPhoto(Bitmap photo) {
+        this.photo = photo;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Etudiant(int id , String nom , String prenom ,  String classe, String phone ,Bitmap photo)
+    {
+        this.id = id;
+        this.nom = nom ;
+        this.prenom = prenom ;
+        this.phone = phone;
+        this.photo = photo ;
+    }
+}
